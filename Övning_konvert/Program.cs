a8803437476ca561477f41e3fa9b85c41e5ec410
lukas <lukas@LUKAS>
2026-09-08 14:57:47 +0200

    Add project files.

---
+++ Övning_konvert/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace Övning_konvert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nivå 1
+            // Ö1
+            //double temp;
+
+            //Console.WriteLine("Skriv in temperaturen");
+            //temp = double.Parse(Console.ReadLine());
+
+            //double fahrenheit = temp * 9 / 5 + 32;
+
+            //Console.WriteLine(fahrenheit);
+
+
+
+            // Ö2
+            //int number;
+
+            //Console.WriteLine("Mata in ett heltal");
+            //bool isConverted = int.TryParse(Console.ReadLine(), out number);
+
+            //if (isConverted)
+            //{
+            //    Console.WriteLine("Bra");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Fel inmatning");
+            //}
+
+
+
+            // Ö3
+            Random number1 = new Random();
+            int number2 = number1.Next(1, 6);
+
+            while(true)
+            {
+
+            }
+            Console.WriteLine(number2);
+
+            // Random
+            //Random ran = new Random();
+            //int number = ran.Next(1, 11);
+
+            //Console.WriteLine(number);
+
+
+
+
+
+
+
+
+        }
+    }
+}
